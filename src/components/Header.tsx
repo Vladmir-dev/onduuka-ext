fa977505353719fff1fcf1f0d4c28e347fad195f
Vladmir-dev <tempestpius70@gmail.com>
2022-05-03 14:53:03 +0300

    changed logo color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,8 +7,8 @@ import StorefrontRoundedIcon from '@mui/icons-material/StorefrontRounded';
 const Header: React.FC = ( ) => {
   return (
     <div className='bg-[#053A67CC] w-25 mr-80 ml-80  h-20 rounded-full text-center  text-3xl text-white flex'>
-        <div className='  bg-[#FFE27A] h-20 rounded-full  text-center w-20 flex items-center justify-center'>
-        <Icon icon="mdi:gift" color="#9d5f36" />
+        <div className='  bg-white h-20 rounded-full  text-center w-20 flex items-center justify-center'>
+        <StorefrontRoundedIcon className='text-[#053A67CC]' />
         </div>
         <p className='p-5'>Hello, Let's quickly finish setting up your Store</p>
     </div>
